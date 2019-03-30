d8a4aa3714fdf6444eec38395da667318e7acda4
coderquill <coderquill@gmail.com>
2019-03-30 11:58:40 +0530

    Quizz app

---
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startQuiz(View v)
+    {
+        Intent intent = new Intent(this, Question1.class);
+
+        startActivity(intent);
+    }
+}
